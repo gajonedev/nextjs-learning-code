8c3d13bea215b2d149bb2b1a179382f8f2882132
Néhémie Gandonou <gajonetronics@gmail.com>
2024-09-13 16:18:38 +0100

    Resetting commit

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -91,7 +91,7 @@ export default function LoginForm() {
           ) : (
             <>
               <Loader />
-              {"Signin in..."}
+              {"Logging in..."}
             </>
           )}
         </Button>
